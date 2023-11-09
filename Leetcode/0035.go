5a505d99e3add313b215d9fd8c8ed2aba35fdcbe
npcxax <npcxax@outlook.com>
2023-11-09 21:14:52 +0800

    11-9

---
+++ Leetcode/0035.go
@@ -0,0 +1,16 @@
+package leetcode
+
+func searchInsert(nums []int, target int) int {
+	var left, right = 0, len(nums) - 1
+	for left < right {
+		mid := (left + right) >> 1
+		if nums[mid] == target {
+			return mid
+		} else if nums[mid] > target {
+			right = mid - 1
+		} else {
+			left = mid + 1
+		}
+	}
+	return left
+}
